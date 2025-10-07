717d1c7a650eb0dbc84ef813b4b21569cc943f5c
Hüseyin Emeci <huseyinemeci@gmail.com>
2025-10-08 00:20:18 +0300

    Fix: recaptcha url

--- config/recaptchav3.php
+++ config/recaptchav3.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'origin' => env('RECAPTCHAV3_ORIGIN', 'https://www.google.com/recaptcha'),
+    'origin' => env('RECAPTCHAV3_ORIGIN', 'https://www.recaptcha.net/recaptcha/api/siteverify'),
     'sitekey' => env('RECAPTCHAV3_SITEKEY', '6LdCSN0rAAAAADurBsdHnynsPtVPd_X2JFfw9AcT'),
     'secret' => env('RECAPTCHAV3_SECRET', '6LdCSN0rAAAAAI-EtbLPfFFnJ4TZ6Uaansk1rsrC'),
     'locale' => env('RECAPTCHAV3_LOCALE', 'en')
